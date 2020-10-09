9498f870830bcbdb7a1839fc205bebebee06e4ed
archennz <an.ran.chen1994@gmail.com>
2020-10-09 18:04:37 +1300

    cleaned up code using Josh's suggestions

--- snake.py
+++ snake.py
@@ -1,4 +1,3 @@
-import sys
 import pygame
 from random import randint
 
@@ -18,14 +17,14 @@ def draw_grid(width, nrow, surface):
 
 class Apple(pygame.sprite.Sprite):
     def __init__(self):
+        """ Apples comes with randomly generated inherent x,y position"""
         pygame.sprite.Sprite.__init__(self)
         self.x = randint(0, nrow-1)
         self.y = randint(0, nrow-1)
-        # self.image = pygame.Surface([width, width])
-        # self.image.fill((255, 0, 0))
         self.rect = pygame.Rect(self.x * width, self.y * width, width, width)
 
     def displace(self, forbidden):
+        """Given a list of forbidden coordinates, moves apple to avilable new coordinates"""
         while (self.x, self.y) in forbidden:
             self.x = randint(0, nrow-1)
             self.y = randint(0, nrow-1)
@@ -35,59 +34,48 @@ class Apple(pygame.sprite.Sprite):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
 
 
-## To do
-# cannot walk into itself
-# apples need to not be able to go to snake
 class Snake(pygame.sprite.Sprite):
     def __init__(self, x, y):
+        """ Given x,y position, assign snake of length 1"""
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.x_dir = 0
         self.y_dir = 1
-        # self.image = pygame.Surface([width, width])
-        # self.image.fill((0,225, 0))
         self.rect = pygame.Rect(self.x * width, self.y * width, width, width)
-        self.tail = [] # elem should be a list of rect
-        self.tail_pos = []
-        
+        self.tail = []  # elem should be a list of rect objects
+        self.tail_pos = []  # elem should be a list of coord (x,y)
 
     def draw(self, surface):
-        """prints whole snake on surface"""
+        """prints snake head and tail on surface"""
         pygame.draw.rect(surface, (0, 255, 0), self.rect)
-        # print("---------------------------")
-        # print(self.rect)
         for rect in self.tail:
             pygame.draw.rect(surface, (0, 0, 255), rect)
-        # print(self.tail)
-        # print("---------------------------")
 
-    def move_head(self):
-        """updates new position of head, given keyboard input"""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.x_dir = -1
-                    self.y_dir = 0
-                elif event.key == pygame.K_RIGHT:
-                    self.x_dir = 1
-                    self.y_dir = 0
-                elif event.key == pygame.K_UP:
-                    self.x_dir = 0
-                    self.y_dir = -1
-                elif event.key == pygame.K_DOWN:
-                    self.x_dir = 0
-                    self.y_dir = 1
+    def move_head(self, orders):
+        """ Given orders 'L' 'R' 'U' 'D', 
+            updates position of snake head in the next frame
+            continue with existing direction if received no orders """
+        if orders == 'L':
+            self.x_dir = -1
+            self.y_dir = 0
+        if orders == 'R':
+            self.x_dir = 1
+            self.y_dir = 0
+        elif orders == 'U':
+            self.x_dir = 0
+            self.y_dir = -1
+        elif orders == 'D':
+            self.x_dir = 0
+            self.y_dir = 1
 
         self.x = (self.x_dir + self.x) % nrow  # ensure the world wraps around
         self.y = (self.y_dir + self.y) % nrow
         self.rect = pygame.Rect(self.x * width, self.y * width, width, width)
 
-
     def move_tail(self, eating = False):
-        """updates new position of tail"""
+        """updates new position of tail
+            grows tail by one if eating"""
         new_tail = [self.rect] + self.tail
         new_tail_pos = [(self.x, self.y)] + self.tail_pos
         if eating == True:
@@ -97,51 +85,76 @@ class Snake(pygame.sprite.Sprite):
             self.tail = new_tail[:-1]
             self.tail_pos = new_tail_pos[:-1]
 
+    def is_tangled(self):
+        """ returns False if snake is moving into itself"""
+        return (self.x, self.y) in self.tail_pos
+
+    def move_snake(self, orders, eating = False):
+        """ update whole snake position depending if snake is eating
+            disallows illegal snake moves"""
+        if self.is_tangled():
+            print("fucked up") # should probably fix this to print something better
+            pygame.quit()
+        else:
+            self.move_tail(eating = eating)
+            self.move_head(orders)
+
+
     def eat(self, apple):
         return pygame.sprite.collide_rect(self, apple)
 
-    def check_tangled(self):
-        return (self.x, self.y) in self.tail_pos
-
-    def move_snake(self, apple):
-        """updates new snake pos"""
-        if self.check_tangled():
-            print("fucked up")
-            pygame.quit()
-        else:
-            eating = Snake.eat(self, apple)
-            self.move_tail(eating = eating)
-            self.move_head()
 
 
-
-
-
-
-def main():
-    # Initialise screen
+def initialise_screen(nrow, width):
+    """makes screen and draw grid"""    
     s_width = nrow*width
     pygame.init()
     screen = pygame.display.set_mode((s_width, s_width))
     pygame.display.set_caption('Basic Snake')
 
-    # Make background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((0, 0, 0))
     draw_grid(width, nrow, background)
 
+    return screen, background
+
+
+def initialise_apple(snake):
+    """ given snake, makes apple not overlapping snake """
+    apple = Apple()
+    forbidden = [(snake.x, snake.y)] + snake.tail_pos
+    apple.displace(forbidden)
+    return apple
+
+def is_eating(snake, apple):
+    """ check if snake head is in same pos as apple """
+    return (snake.x, snake.y) == (apple.x, apple.y)
+
+def get_control():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                return 'L'
+            elif event.key == pygame.K_RIGHT:
+                return 'R'
+            elif event.key == pygame.K_UP:
+                return 'U'
+            elif event.key == pygame.K_DOWN:
+                return 'D'
+
+def main():
+
+    screen, background = initialise_screen(nrow, width)
 
     # Make snake
     snake = Snake(8,1)
 
-    # Make apple 
-    apple = Apple()
-    forbidden = [(snake.x, snake.y)] + snake.tail_pos
-    apple.displace(forbidden)
-    #snake.tail = [pygame.Rect(snake.x * width, (snake.y+1) * width, width, width),
-    #                pygame.Rect(snake.x * width, (snake.y+2) * width, width, width)]
-    #snake.tail = []
+    # Make apple
+    apple = initialise_apple(snake)
+
     # Initialise clock
     clock = pygame.time.Clock()
 
@@ -152,10 +165,10 @@ def main():
         # draws apple
         apple.draw(screen)
 
-        eaten = snake.eat(apple)
+        eaten = is_eating(snake, apple)
 
         # checks if snake touching apple and update next 
-        snake.move_snake(apple)
+        snake.move_snake(get_control(), eating = eaten)
 
         if eaten:
             forbidden = [(snake.x, snake.y)] + snake.tail_pos
@@ -163,8 +176,6 @@ def main():
 
         # draws snake and tail
         snake.draw(screen)
-
-
 
         # if snake.eat(apple):
         #   
