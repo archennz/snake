0b40e7a7c0422f0f6c2a2ec2b193e2cd238533dd
archennz <an.ran.chen1994@gmail.com>
2020-10-22 12:14:34 +1300

    finished work on hex coord, implemented two debug functions with plt

---
+++ game/hex_snake.py
@@ -0,0 +1,77 @@
+from game.world import nrow, width
+from math import cos, sin, radians
+import matplotlib.pyplot as plt
+
+
+def change_coord(coord):
+    """change coord from column, row to game coord n,m
+    expect x to be in range(0, nrow)
+    y to be in range (0, nrow)"""
+    (column, row) = coord
+    offset = row//2
+    if column >= (nrow - offset):
+        n = column - nrow
+        return (n, row)
+    return (column, row)
+
+
+def draw_game_coord(game_coord):
+    """render game coord into game coord on plane"""
+    (column, row) = game_coord
+    cross_width = cos(radians(30))*width
+    game_x = cross_width/2 + row*cross_width/2 + column*cross_width
+    game_y = width/2 + row*cross_width*cos(radians(30))
+    return (game_x, game_y)
+
+
+def make_hex_points(game_coord, inner_rad):
+    """draws the six pts on the hex surrounding center"""
+    (m, n) = game_coord
+    (game_x, game_y) = draw_game_coord(game_coord)
+    points = []
+    for i in range(-30, 330, 60):
+        new_x = game_x + inner_rad*cos(radians(i))
+        new_y = game_y + inner_rad*sin(radians(i))
+        points.append((new_x, new_y))
+    return points
+
+
+def draw_game_center():
+    """debugging by drawing the centers of each hexagon
+    in the game world"""
+    xs = []
+    ys = []
+    for i in range(nrow):
+        for j in range(nrow):
+            game_coord = change_coord((i, j))
+            (plot_x, plot_y) = draw_game_coord(game_coord)
+            xs.append(plot_x)
+            ys.append(plot_y)
+    plt.scatter(xs, ys)
+    plt.show()
+
+
+def draw_game_hex():
+    """debugging by drawing the centers and hex edges
+    in the game world"""
+    inner_rad = width*(1/3)
+    xs = []
+    xs_pts = []
+    ys = []
+    ys_pts = []
+    for i in range(nrow):
+        for j in range(nrow):
+            game_coord = change_coord((i, j))
+            (plot_x, plot_y) = draw_game_coord(game_coord)
+            points = make_hex_points(game_coord, inner_rad)
+            for (x, y) in points:
+                xs_pts.append(x)
+                ys_pts.append(y)
+            xs.append(plot_x)
+            ys.append(plot_y)
+    plt.scatter(xs, ys)
+    plt.scatter(xs_pts, ys_pts, marker='*')
+    plt.show()
+
+
+if __name__ == '__main__': draw_game_hex()
---
+++ test/test_hex_snake.py
@@ -0,0 +1,33 @@
+from game import hex_snake
+from game.world import nrow
+import pytest
+
+
+class TestChangeCoord:
+    # need a minimum bound on nrow
+    @pytest.mark.parametrize('x_coord, game_coord', [
+        # first row
+        ((0, 0), (0, 0)),
+        ((1, 0), (1, 0)),
+        ((nrow-1, 0), (nrow-1, 0)),
+        ((nrow-2, 0), (nrow-2, 0)),
+        # second row
+        ((0, 1), (0, 1)),
+        ((1, 1), (1, 1)),
+        ((nrow-1, 1), (nrow-1, 1)),
+        ((nrow-2, 1), (nrow-2, 1)),
+        # third row
+        ((0, 2), (0, 2)),
+        ((1, 2), (1, 2)),
+        ((nrow-1, 2), (-1, 2)),
+        ((nrow-2, 2), (nrow-2, 2)),
+        # fourth row
+        ((0, 3), (0, 3)),
+        ((1, 3), (1, 3)),
+        ((nrow-1, 3), (-1, 3)),
+        ((nrow-2, 3), (nrow-2, 3))
+    ])
+    def test_change_coord(self, x_coord, game_coord):
+        assert hex_snake.change_coord(x_coord) == game_coord
+
+        
