ab27c70750a78fddef8c67e77aab6f5fb0f9f473
archennz <an.ran.chen1994@gmail.com>
2020-10-21 14:56:16 +1300

    reorganized test files

--- game/snake.py
+++ game/snake.py
@@ -1,7 +1,7 @@
 import pygame
 from game.world import nrow, width  # import size of world
 from players import Apple, Snake
-=)
+
 
 def draw_grid(width, nrow, surface):
     """creates chessgrid for surface"""
